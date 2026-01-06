197abf6cbe95b5468de4ff902956c8a1c2c50f2d
Marcin <m.devkontakt@gmail.com>
2026-01-06 19:02:14 +0100

    fix manifest icon path

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#000000',
         icons: [
             {
-                src: '/icon',
+                src: '/icon.png',
                 sizes: 'any',
                 type: 'image/png',
             },
